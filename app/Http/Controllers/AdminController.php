ca8ea6be02fec8ba2d3570e71e81bb9350ba489b
Mona1009 <96369156+Mona1009@users.noreply.github.com>
2022-01-04 01:14:29 -0800

    commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Doctor;
+
+use App\Models\Appointment;
+
+
+
+class AdminController extends Controller
+{
+    public function addview()
+    {
+
+        return view('admin.add_doctor');
+    }
+    public function upload(Request $request)
+    {
+        $doctor=new doctor;
+
+        $image=$request->file;
+
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+
+
+        $request->file->move('doctorimage',$imagename);
+
+        $doctor->image=$imagename;
+
+
+        $doctor->name=$request->name;
+
+        $doctor->phone=$request->number;
+
+        $doctor->room=$request->room;
+
+        $doctor->speciality=$request->speciality;
+
+
+        $doctor->save();
+
+        return redirect()->back()->with('message','Doctor Added Successfully');
+
+    }
+
+    public function showappointment()
+    {
+        $data=appointment::all();
+
+
+        return view('admin.showappointment',compact('data'));
+    }
+
+
+    public function approved($id)
+    {
+        $data=appointment::find($id);
+
+        $data->status='Approved';
+
+        $data->save();
+
+        return redirect()->back();
+    }
+
+
+    public function canceled($id)
+    {
+        $data=appointment::find($id);
+
+        $data->status='Canceled';
+
+        $data->save();
+
+        return redirect()->back();
+    }
+
+}
